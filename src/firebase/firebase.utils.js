0eb8ea435094dfbf9f984d31ea3c2e9b6d4409b4
Samantha Eastwood <sam.eastwood.uk@googlemail.com>
2021-05-15 12:36:44 +0100

    add sign up form and save details to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import swal from 'sweetalert';
+ 
 
 const config = {
     apiKey: "AIzaSyBdkpmUuHgjH8OgXaiiFbGl0_RPoRU8dx0",
@@ -12,6 +14,35 @@ const config = {
   };
 
   firebase.initializeApp(config);
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    
+    const snapShot = await userRef.get();
+    
+    console.log(snapShot);
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        swal('Error creating user', error.message);
+      }
+    } else {
+        swal("User account already exists");
+    }
+  
+    return userRef;
+  };
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
